4ed8d86ed1f400e5dde70a3867c92adea76c449e
ISHITA JAIN <67500673+ishita152001@users.noreply.github.com>
2022-11-11 14:54:17 +0530

    added

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+// import link
+import { Link } from 'react-router-dom';
+// import logo
+import Logo from '../assets/img/logoPic.png';
+
+const Header = () => {
+  return (
+    <header className='py-6 mb-12 border-b'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <Link to='/'>
+          <img src={Logo} alt='' style={{height:"80px"}} />
+        </Link>
+        <div className='flex items-center gap-6'>
+          <Link className='hover:blue-900 transition' to='/'>
+            Log in
+          </Link>
+          <Link
+            className='bg-blue-700 hover:bg-blue-800 text-white px-4 py-3 rounded-lg transition'
+            to='/'
+          >
+            Sign up
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
